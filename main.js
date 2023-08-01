58625df0077887d3f37ef440c58b47b5820bcdbf
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-01 17:47:19 -0400

    Add querySelectors for both players

---
+++ main.js
@@ -0,0 +1,14 @@
+// querySelectors
+var humanPlayer = document.querySelector(".human-player")
+var computerPlayer = document.querySelector(".computer-player")
+
+// eventListeners
+
+// global variables
+
+// functions
+
+
+
+
+
