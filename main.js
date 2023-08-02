fc0a766955a933ab7528cf510db67acde5b9858b
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-02 09:48:38 -0400

    Add pseudocode for createGame function

--- main.js
+++ main.js
@@ -10,17 +10,47 @@ classicGameOption.addEventListener("click", function(){
 })
 // global variables
 
+var gameBoard;
+// assign gameboard to gameBoard function?
+
 // functions
 
 
-function createPlayer(name, token, wins) {
+function createPlayer(name, token) {
   var player = {
     name: name,
     token: token,
-    wins: wins
+    wins: 0
   }
   return player
 }
+
+function createGame(player1, player2) {
+  // track the players turn
+  // keep track of the plays
+  // the game type
+  // that will tell us which game to turn
+  // call takeTurn function
+}
+
+// function takeTurn() {
+
+// }
+
+// GAME LOGIC
+// rock beats scissors, scissors beat paper, and paper beats rock.
+
+// if player1 is rock and player2 is scissors, player1 wins
+        // else if player2 is paper, player2 wins
+            // else if player2 is 
+// if player1 is scissors and player2 is paper, player1 wins
+      //  else if player2
+// if player1  is paper and player2 is rock, player1 wins
+
+
+
+
+
 
 function hideElement(element) {
   element.classList.add("hidden")
